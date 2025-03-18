64dd8148b3da97fa9c4b4450db60038e17df1444
iamravinder1629 <iamravinder1629@gmail.com>
2025-03-18 17:13:52 +0530

    post model changed

--- models/postMoel.js
+++ models/postMoel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     title:
     {
-        type: String,
+        type: Number,
         required: true
     },
     discription:
